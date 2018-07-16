79690ec640d8190787366d9a46080ff1e025c78f
Aurel4012 <33459190+Aurel4012@users.noreply.github.com>
2018-07-16 11:39:05 +0200

    encode data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,21 +7,34 @@ import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-sca
 })
 export class HomePage {
   scanData : {};
+  encodeData : string ;
+  encodedData : {} ;
   options :BarcodeScannerOptions;
   constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
     
   }
-  scan(){
-    this.options = {
-        prompt : "Scan du code barre "
-    }
-    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+    scan(){
+        this.options = {
+            prompt : "Scan du code barre "
+        }
+        this.barcodeScanner.scan(this.options).then((barcodeData) => {
 
-        console.log(barcodeData);
-        this.scanData = barcodeData;
-    }, (err) => {
-        console.log("Error occured : " + err);
-    });         
-}    
+            console.log(barcodeData);
+            this.scanData = barcodeData;
+        }, (err) => {
+            console.log("Error occured : " + err);
+        });         
+    }    
+
+    encodeText(){
+        this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodeData).then((encodedData) => {
+    
+            console.log(encodedData);
+            this.encodedData = encodedData;
+    
+        }, (err) => {
+            console.log("Error occured : " + err);
+        });                 
+    }
 }
  
