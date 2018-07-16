d0f1084e716d20a98daae462ffff5bd30fb47f48
Aurel4012 <33459190+Aurel4012@users.noreply.github.com>
2018-07-16 11:16:38 +0200

    barcode ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  scanData : {};
+  options :BarcodeScannerOptions;
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+    
   }
+  scan(){
+    this.options = {
+        prompt : "Scan du code barre "
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
 
+        console.log(barcodeData);
+        this.scanData = barcodeData;
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });         
+}    
 }
+ 
